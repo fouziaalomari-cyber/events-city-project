aca2156f8d5654a238026b397d72ee92da479954
fawzia 1995 <ramromio1995@gmail.com>
2026-07-19 21:01:53 +0300

    عرض أخطاء PHP مؤقتًا للتصحيح

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+// Temporary: enable detailed PHP errors for debugging on Railway. Remove when fixed.
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/db.php';
 
 $basePath = '';
